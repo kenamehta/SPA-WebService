ed4dff08b327d81382a55e5d4f7c6b6f6f7b64b4
kenamehta <kenamehta1995@gmail.com>
2020-03-26 23:33:32 -0700

    changed index.js to app.js

--- app.js
+++ app.js
@@ -56,9 +56,12 @@ app.post("/patients/add", async (req, res) => {
     }
     res.redirect("/");
   });
-  
 });
 
-app.listen(3000, () => {
-  console.log(`Started the port at 3000`);
+app.listen(process.env.PORT || 3000, function() {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
